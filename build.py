c2c7f442b7abdbaa2ebc02f899b27ff0952762bc
Evan Wallace <evan@evan-wallaces-macbook-pro.local>
2011-06-01 14:10:52 -0400

    added compression of glsl strings in release

--- build.py
+++ build.py
@@ -13,16 +13,33 @@ def sources():
 def compile(sources):
     return '\n'.join('// %s\n%s' % (path, open(path).read()) for path in sources)
 
+def compress_glsl(text):
+    def compress(match):
+        text = match.group(0)
+        if '  ' in text: # assume all strings with two consecutive spaces are glsl
+            text = re.sub('/\*.*?\*/', '', text) # remove all comments
+            text = re.sub(' +', ' ', text) # replace consecutive spaces with one space
+            text = re.sub(r' ?(\+|\-|\*|/|,|=|{|}|;|\(|\)|<|>|!|\'|\") ?', r'\1', text) # tighten spaces around some tokens
+        return text
+
+    text = re.sub(r"'([^'\\]|\\(.|\n))*'", compress, text) # replace all single-quoted strings
+    text = re.sub(r'"([^"\\]|\\(.|\n))*"', compress, text) # replace all double-quoted strings
+    return text
+
 def build():
-    data = ('var %s = (function() {\nvar exports = {};\n\n' + compile(sources()) + '\nreturn exports;\n})();\n') % module
+    data = 'var %s = (function() {\nvar exports = {};\n\n' % module + compile(sources()) + '\nreturn exports;\n})();\n'
     if 'release' in sys.argv:
-        f, temp_path = tempfile.mkstemp()
-        os.write(f, data)
-        os.close(f)
-        os.system('closure --js %s --js_output_file %s' % (temp_path, output_path))
-        os.remove(temp_path)
-    else:
-        open(output_path, 'w').write(data)
+        f1, temp1_path = tempfile.mkstemp()
+        f2, temp2_path = tempfile.mkstemp()
+        os.write(f1, data)
+        os.close(f1)
+        os.close(f2)
+        os.system('closure --js %s --js_output_file %s' % (temp1_path, temp2_path))
+        os.remove(temp1_path)
+        data = open(temp2_path).read()
+        os.remove(temp2_path)
+        data = compress_glsl(data)
+    open(output_path, 'w').write(data)
     print 'built %s (%u lines)' % (output_path, len(data.split('\n')))
 
 def stat():
